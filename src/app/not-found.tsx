703da329f423385e72a21c98ab4675e08f4a590a
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-08-13 13:59:15 -0400

    Add files via upload

---
+++ src/app/not-found.tsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+    return (
+      <div>
+        <h1>404 - Not Found</h1>
+        <p>The page you are looking for could not be found.</p>
+      </div>
+    );
+  }
