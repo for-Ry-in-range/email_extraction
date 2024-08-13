02fad5ff81efb643665cbe16f67d3f2ea7f3deab
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-08-13 14:54:14 -0400

    Delete src/app/not-found.tsx

--- src/app/not-found.tsx
+++
@@ -1,8 +0,0 @@
-export default function NotFound() {
-    return (
-      <div>
-        <h1>404 - Not Found</h1>
-        <p>The page you are looking for could not be found.</p>
-      </div>
-    );
-  }
