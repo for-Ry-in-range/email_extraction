ec7bac0f7171b719487ee27bd75c633976f6a571
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-08-15 17:43:22 -0400

    Update route.ts

--- src/app/api/cron/route.ts
+++ src/app/api/cron/route.ts
@@ -1,12 +1,10 @@
 import { inngest } from "../../../../inngest/client";
 
 export async function POST() {
-    console.log("GETTTT")
     try {
         await inngest.send({
           name: "myfunc/send-summary"
         });
-        console.log("After send-summary")
         return new Response("Successful!", { status: 200 });
     } catch (error) {
         console.error("Error:", error)
