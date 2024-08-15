251da7207d314ffc9ea0882920b47ccc51a17b63
Ryan Wong <108443873+for-Ry-in-range@users.noreply.github.com>
2024-08-15 14:07:43 -0400

    Add files via upload

---
+++ src/app/api/create_inbox/route.ts
@@ -0,0 +1,27 @@
+import { inngest } from "../../../../inngest/client";
+import { IncomingMessage } from 'http';
+
+export async function POST(req: IncomingMessage) {
+    let parsedBody: any = null;
+    if (req.headers['Content-Type'] === 'application/json') {
+        let body = '';
+        (req as any).on('data', (chunk: { toString: () => string; }) => {
+            body += chunk.toString();
+        });
+        (req as any).on('end', () => {
+            parsedBody = JSON.parse(body);
+        });
+        if (parsedBody) {
+            await inngest.send({
+                name: "myfunc/create-inbox",
+                data: {
+                    name: parsedBody.name,
+                    prompt: parsedBody.prompt,
+                    send_to: parsedBody.send_to
+                }
+            });
+        } else {
+            console.error("Missing data in request body");
+        }
+    }
+}
